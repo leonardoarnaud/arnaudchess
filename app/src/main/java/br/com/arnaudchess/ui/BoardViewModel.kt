1d1a29f7c5f698b6a5b1dd0ec37cff0746ae01ec
Leonardo B. Arnaud <leonardo.arnaud.java@gmail.com>
2020-07-30 01:40:59 -0300

    movimentação do bispo

--- app/src/main/java/br/com/arnaudchess/ui/BoardViewModel.kt
+++ app/src/main/java/br/com/arnaudchess/ui/BoardViewModel.kt
@@ -48,6 +48,19 @@ class BoardViewModel : ViewModel() {
         add(arrayListOf(_a1, _b2, _c3, _d4, _e5, _f6, _g7, _h8))
         add(arrayListOf(_b1, _c2, _d3, _e4, _f5, _g6, _h7, _i8))
         add(arrayListOf(_c1, _d2, _e3, _f4, _g5, _h6, _i7, _j8))
+        add(arrayListOf(_d1, _e2, _f3, _g4, _h5, _i6, _j7))
+        add(arrayListOf(_e1, _f2, _g3, _h4, _i5, _j6))
+        add(arrayListOf(_f1, _g2, _h3, _i4, _j5))
+        add(arrayListOf(_g1, _h2, _i3, _j4))
+        add(arrayListOf(_h1, _i2, _j3))
+        add(arrayListOf(_i1, _j2))
+
+        add(arrayListOf(_j2, _i3, _h4, _g5, _f6, _e7, _d8))
+        add(arrayListOf(_j3, _i4, _h5, _g6, _f7, _e8))
+        add(arrayListOf(_j4, _i5, _h6, _g7, _f8))
+        add(arrayListOf(_j5, _i6, _h7, _g8))
+        add(arrayListOf(_j6, _i7, _h8))
+        add(arrayListOf(_j7, _i8))
     }
 
     val capturedPieces = ArrayList<Piece>()
@@ -69,7 +82,7 @@ class BoardViewModel : ViewModel() {
             put(_b2, Pawn(WHITE))
             put(_c2, Pawn(WHITE))
             put(_d2, Knight(WHITE))
-            put(_e2, Swordsman(WHITE))
+            //put(_e2, Swordsman(WHITE))
             put(_f2, Swordsman(WHITE))
             put(_g2, Knight(WHITE))
             put(_h2, Pawn(WHITE))
